e1af8d3c347d49ff391367dc6872ce43470b4489
morispolanco <morispolanco@gmail.com>
2023-09-07 19:35:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ def generar_analisis_factibilidad(idea_negocio, capital_inicial, tiempo_retorno)
     # Realizar un análisis completo de factibilidad utilizando GPT-3 de OpenAI
     try:
         respuesta_gpt3_analisis_factibilidad = openai.Completion.create(
-            engine="text-davinci-003",
+            engine="text-davinci-002",
             prompt=f"Analiza la factibilidad de la siguiente idea de negocio:\n\n'{idea_negocio}'\n\nCapital Inicial: ${capital_inicial}\nTiempo de Retorno Esperado: {tiempo_retorno} meses.",
             max_tokens=1000
         )
@@ -25,26 +25,21 @@ st.sidebar.header("Configuración del Negocio")
 capital_inicial = st.sidebar.number_input("Capital Inicial (en dólares)", min_value=0)
 tiempo_retorno = st.sidebar.number_input("Tiempo de Retorno Esperado (en meses)", min_value=1)
 
-# Verificar si se ha ingresado una clave de API válida y se han proporcionado valores para capital inicial y tiempo de retorno
-if api_key and capital_inicial > 0 and tiempo_retorno >= 1:
+# Permitir al usuario ingresar su propia idea de negocio
+st.sidebar.header("Ingresar Idea de Negocio")
+idea_negocio = st.sidebar.text_area("Ingresa tu Idea de Negocio")
+
+# Verificar si se ha ingresado una clave de API válida y se han proporcionado valores para capital inicial, tiempo de retorno y una idea de negocio
+if api_key and capital_inicial > 0 and tiempo_retorno >= 1 and idea_negocio:
     # Inicializar la API de OpenAI
     openai.api_key = api_key
 
     # Título de la aplicación
     st.title("Generador de Análisis de Factibilidad de Negocio")
 
-    # Generar una idea de negocio utilizando GPT-3 de OpenAI
-    st.header("Sugerencia de Idea de Negocio:")
-    try:
-        respuesta_gpt3_idea = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt="Genera una idea de negocio innovadora y factible.",
-            max_tokens=1000
-        )
-        idea_negocio = respuesta_gpt3_idea.choices[0].text.strip()
-        st.write(idea_negocio)
-    except Exception as e:
-        st.error("Se produjo un error al generar la idea de negocio. Verifica tu clave de API y vuelve a intentarlo.")
+    # Mostrar la idea de negocio ingresada por el usuario
+    st.header("Idea de Negocio:")
+    st.write(idea_negocio)
 
     # Botón para generar un análisis completo de factibilidad
     if st.button("Generar Análisis de Factibilidad"):
@@ -55,4 +50,4 @@ if api_key and capital_inicial > 0 and tiempo_retorno >= 1:
         st.write(analisis_factibilidad)
 
 else:
-    st.warning("Por favor, completa la configuración de API, el capital inicial y el tiempo de retorno en el panel izquierdo.")
+    st.warning("Por favor, completa la configuración de API, el capital inicial, el tiempo de retorno y proporciona una idea de negocio en el panel izquierdo.")
